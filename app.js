61698d7def8f00256e407365638ade4a12c37464
Zawahir Ahmed <zawahir123ahmed123pk@gmail.com>
2024-03-18 22:35:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,122 @@
+// ============QUESTION 01=====================================
+
+// var myArray = [];
+
+// myArray.push("zawahir");
+// myArray.push("zawahir");
+// myArray.push(19);
+
+// console.log(myArray[0]); 
+// console.log(myArray[1]); 
+// console.log(myArray[2]);
+
+// ============QUESTION 02=====================================
+
+// var studentNamesArray = {
+//     firstname: ["zawahur"],
+//     last: ["umer"],
+//     Age: [19]
+// };
+// console.log(studentNamesArray); 
+
+// ============QUESTION 03=====================================
+
+// var stringsArray = ["zawahir", "azhan", "Omer", "zayan"];
+// console.log(stringsArray);
+
+// ============QUESTION 04=====================================
+// var intArray = ["19", "14", "17", "7"];
+// console.log(intArray);
+
+// ============QUESTION 05=====================================
+
+// let booleanArray = [true, false, true, true, false];
+// console.log(booleanArray);
+
+// ============QUESTION 06=====================================
+
+// let mixedArray = ['zawahir', 19, true, { name: 'azhan', age: 17 }, null];
+// console.log(mixedArray);
+
+
+// ============QUESTION 07=====================================
+
+// var names = [];
+// console.log(names);
+ 
+// var qualifications = [
+//     '01) SSC - Secondary School Certificate',
+//     '02) HSC - Higher Secondary Certificate',
+//     '03) BCS - Bachelor of Computer Science',
+//     '04) BS - Bachelor of Science',
+//     '05) BCOM - Bachelor of Commerce',
+//     '06) MS - Master of Science',
+//     '07) M. Phil. - Master of Philosophy',
+//     '08) PhD - Doctor of Philosophy'
+
+// ======================questions08==================================
+
+// function calculatePercentage(score, totalMarks) {
+//     return (score / totalMarks) * 100;
+// }
+
+// var studentNames = ["zawahir", "azhan", "zayan"];
+
+// var studentScores = [420, 380, 450]; 
+
+// const totalMarks = 500;
+
+// for (var i = 0; i < studentNames.length; i++) {
+//     var percentage = calculatePercentage(studentScores[i], totalMarks);
+//     document.writeln(`<br>${studentNames[i]} scored ${studentScores[i]} out of ${totalMarks}, which is ${percentage}%`);
+// }
+
+// ======================questions10==================================
+
+// var studentScores = [90, 85, 70, 95, 80];
+
+// document.writeln("Original array:", studentScores);
+
+// studentScores.sort(function(a, b) {
+//     return a - b; 
+// });
+
+// document.writeln("<br>Sorted array:", studentScores);
+
+// ======================questions11==================================
+
+// const cities = ['New York', 'London', 'Paris', 'Tokyo', 'Sydney'];
+
+// const selectedCities = [];
+
+// for (let i = 0; i < 3; i++) {
+//     if (i < cities.length) {
+//         selectedCities.push(cities[i]);
+//     } else {
+//         break; 
+//     }
+// }
+// document.writeln("Selected Cities:", selectedCities);
+
+// ======================questions14==================================
+
+// var stack = [];
+
+// stack.push("out");
+// stack.push("monitor");
+// stack.push("out");
+// stack.push("printer");
+// stack.push("out");
+// stack.push("mouse");
+// stack.push("keyboard");
+
+// while (stack.length > 0) {
+//     var value = stack.pop();
+//     console.log(value); 
+// }
+
+// ]; 
+
+// for (var i = 0; i < qualifications.length; i++) {
+//     document.writeln(qualifications[i] + "<br>");
+// }
